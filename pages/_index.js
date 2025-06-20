0c47e70dbf425353b7d560219cc801b0cf88f68f
Jyaxx <madhurjya1200@gmail.com>
2025-06-20 21:08:49 +0530

    Update _index.js

--- pages/_index.js
+++ pages/_index.js
@@ -41,7 +41,7 @@ export default function Home() {
   const formatTime = (time) => {
     const minutes = Math.floor(time / 60)
     const seconds = time % 60
-    return \`\${minutes.toString().padStart(2, '0')}:\${seconds.toString().padStart(2, '0')}\`
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
   }
 
   return (
